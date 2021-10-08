73acd02ac8a5684574018e49febf93780c0ef720
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2021-10-08 22:56:06 +1100

    [miryoku] Replace layer widget with Miryoku logo

--- app/boards/arm/corne-ish_zen/custom_status_screen.c
+++ app/boards/arm/corne-ish_zen/custom_status_screen.c
@@ -14,6 +14,7 @@
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 LV_IMG_DECLARE(zenlogo);
+LV_IMG_DECLARE(miryokulogo);
 LV_IMG_DECLARE(layers2);
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BATTERY_STATUS)
@@ -36,36 +37,34 @@ lv_obj_t *zmk_display_status_screen() {
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BATTERY_STATUS)
     zmk_widget_battery_status_init(&battery_status_widget, screen);
-    lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), NULL, LV_ALIGN_IN_TOP_MID,
-                 0, 2);
+    lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), NULL, LV_ALIGN_CENTER, 0, -43);
 #endif
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_OUTPUT_STATUS)
     zmk_widget_output_status_init(&output_status_widget, screen);
-    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), NULL, LV_ALIGN_IN_TOP_MID, 0,
-                 41);
+    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), NULL, LV_ALIGN_CENTER, 0,
+                 0);
 #endif
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
     //lv_style_set_pad_inner(&layerstyle, LV_STATE_DEFAULT, 12);
     //lv_obj_add_style(&layer_status_widget, LV_WIDGET_PART_MAIN, &layerstyle);
     zmk_widget_layer_status_init(&layer_status_widget, screen);
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), NULL, LV_ALIGN_IN_BOTTOM_MID, 0,
-                 -5);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), NULL, LV_ALIGN_CENTER, 0, 43);
 #endif
 
 #if CONFIG_BOARD_CORNEISH_ZEN_RIGHT
     lv_obj_t * zenlogo_icon;
     zenlogo_icon = lv_img_create(screen, NULL);
     lv_img_set_src(zenlogo_icon, &zenlogo);
-    lv_obj_align(zenlogo_icon, NULL, LV_ALIGN_IN_BOTTOM_MID, 2, -5);
+    lv_obj_align(zenlogo_icon, NULL, LV_ALIGN_CENTER, 0, 43);
 #endif
 
 #if CONFIG_BOARD_CORNEISH_ZEN_LEFT
-    lv_obj_t * LayersHeading;
-    LayersHeading = lv_img_create(screen, NULL);
-    lv_obj_align(LayersHeading, NULL, LV_ALIGN_IN_BOTTOM_MID, 8, 5);
-    lv_img_set_src(LayersHeading, &layers2);
+    lv_obj_t * miryokulogo_icon;
+    miryokulogo_icon = lv_img_create(screen, NULL);
+    lv_img_set_src(miryokulogo_icon, &miryokulogo);
+    lv_obj_align(miryokulogo_icon, NULL, LV_ALIGN_CENTER, 0, 43);
 #endif
 
     //lv_task_handler();
